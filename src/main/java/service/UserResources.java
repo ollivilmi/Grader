022d05beef155027baccbdfa3908380affa23b52
Vilmi <olli.vilmi@nokia.com>
2018-06-26 16:02:32 +0300

    Get current user

--- src/main/java/service/UserResources.java
+++ src/main/java/service/UserResources.java
@@ -5,10 +5,10 @@ import org.apache.commons.math3.util.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 import service.entities.UserRepository;
 import service.security.Authorization;
 
@@ -28,6 +28,17 @@ public class UserResources {
     public ResponseEntity register(@RequestBody RegisterForm form){
         Pair<Boolean, String> headers = auth.register(form);
         return new ResponseEntity(headers.getValue(), headers.getKey() ? HttpStatus.CREATED : HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping(path="/getCurrentUserName")
+    public String getCurrentUserName()
+    {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUserName = authentication.getName();
+            return currentUserName;
+        }
+        return null;
     }
 
 }
