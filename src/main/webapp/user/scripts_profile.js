10e53b189112a65186b5730451b5e0493a0413e3
Vilmi <olli.vilmi@nokia.com>
2018-06-27 11:04:26 +0300

    JSON library, auth failure message

--- src/main/webapp/user/scripts_profile.js
+++ src/main/webapp/user/scripts_profile.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
     $.getJSON("/userApi/getCurrentUserName").always(function(response) {
-    $('#welcomeUser').html(response + "'s profile"); });
+    $('#welcomeUser').html(response["username"] + "'s profile"); });
 
 });
